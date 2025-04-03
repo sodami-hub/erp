ff8dc264a83f29c7cbed2617c5d4b5ecf139e568
goo4ram@naver.com <goo4ram@naver.com>
2025-04-03 20:43:37 +0900

    메인 화면 기본 구성 +

---
+++ client/src/routes/LandingPage/index.tsx
@@ -0,0 +1,9 @@
+export default function LandingPage() {
+  return (
+    <section className={'text-gray-400 text-center'}>
+      <h1>여기에 초기 화면 보여주세요.</h1>
+      <h2>This is LandingPage</h2>
+      <h3>Outlet 영역!</h3>
+    </section>
+  );
+}
--- client/src/routes/RoutesSetup.tsx
+++ client/src/routes/RoutesSetup.tsx
@@ -1,10 +1,15 @@
 import {Routes, Route} from 'react-router-dom'
 import NoMatch from './NoMatch'
+import Layout from './Layout/MainFrame'
+import LandingPage from "./LandingPage";
 
 export default function RoutesSetup() {
   return (
     <Routes>
-      <Route path="*" element={<NoMatch />} />
+      <Route path="/" element={<Layout />} >
+        <Route index element={<LandingPage/>}/>
+        <Route path="*" element={<NoMatch/>} />
+      </Route>
     </Routes>
   )
 }
