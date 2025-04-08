e86bd5252c49cf1bd2d4a2888d0cdb92b1e1d130
good4ram@naver.com <good4ram@naver.com>
2025-04-08 15:30:09 +0900

    클라이언트 로그인 페이지, 기능 추가

--- client/src/routes/Auth/Login.tsx
+++ client/src/routes/Auth/Login.tsx
@@ -1,3 +1,87 @@
 import {ChangeEvent, useEffect} from 'react';
 import {useState, useCallback} from 'react';
 import {Link, useNavigate} from 'react-router-dom';
+import {useAuth} from '../../context';
+import * as U from '../../utils';
+
+/*
+{
+  organizationId: string;
+  id: string;
+  password: string;
+}
+ */
+type LoginFormType = Record<'organizationId' | 'id' | 'password', string>;
+const initialFormState = {organizationId: '', id: '', password: ''};
+
+export default function Login() {
+  const [{organizationId, id, password}, setForm] =
+    useState<LoginFormType>(initialFormState);
+
+  const changed = useCallback(
+    (key: string) => (e: ChangeEvent<HTMLInputElement>) => {
+      setForm(obj => ({...obj, [key]: e.target.value}));
+    },
+    []
+  );
+
+  const navigate = useNavigate();
+  const {login} = useAuth();
+  const loginAccount = useCallback(() => {
+    console.log(organizationId, id, password);
+    login(organizationId, id, password, () => navigate('/index'));
+  }, [id, password, organizationId, login]);
+
+  // 저장된 로그인 데이터가 있으면 불러와서 form에 넣기
+  useEffect(() => {
+    U.readObjectP<LoginFormType>('user')
+      .then(user => {
+        if (user) setForm(user);
+      })
+      .catch(e => console.log(e));
+  }, []);
+
+  return (
+    <div className={'w-screen h-screen bg-white m-2 border-2 border-gray-800'}>
+      <div className={'w-full h-10 border-b border-gray-800'}></div>
+      <div
+        className={'h-full w-full bg-gray-200 flex flex-col justify-center items-center'}>
+        <div className={'bg-gray-500 text-3xl text-center w-1/5 h-1/4 mb-8'}>로고</div>
+
+        <div className={'w-1/3 px-6 py-8 text-gray-400 bg-white rounded shadow-md'}>
+          <h1 className={'mb-8 text-2xl text-center text-primary'}>Login</h1>
+          <input
+            type={'text'}
+            className={'w-full p-3 mb-4 input input-primary'}
+            name={'organizationId'}
+            placeholder={'요양기관번호'}
+            value={organizationId}
+            onChange={changed('organizationId')}
+          />
+          <input
+            type={'text'}
+            className={'w-full p-3 mb-4 input input-primary'}
+            name={'id'}
+            placeholder={'아이디'}
+            value={id}
+            onChange={changed('id')}
+          />
+          <input
+            type={'password'}
+            className={'w-full p-3 mb-4 input input-primary'}
+            name={'password'}
+            placeholder={'비밀번호'}
+            value={password}
+            onChange={changed('password')}
+          />
+          <button
+            type={'submit'}
+            className={'w-1/2 ml-28 btn btn-primary'}
+            onClick={loginAccount}>
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- client/src/routes/RoutesSetup.tsx
+++ client/src/routes/RoutesSetup.tsx
@@ -2,10 +2,12 @@ import {Routes, Route} from 'react-router-dom';
 import NoMatch from './NoMatch';
 import Layout from './Layout/MainFrame';
 import LandingPage from './LandingPage';
+import Login from './Auth/Login';
 
 export default function RoutesSetup() {
   return (
     <Routes>
+      <Route path={'/'} element={<Login />} />
       <Route path="/index" element={<Layout />}>
         <Route index element={<LandingPage />} />
         <Route path="*" element={<NoMatch />} />
