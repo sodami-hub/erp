de8593cd447fbd8a1544d5fb21b0d8e49c030c52
good4ram@naver.com <good4ram@naver.com>
2025-04-10 00:47:41 +0900

    server // 스프링부트로 교체

--- server/.prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    bracketSpacing:false,
-    jsxBracketSameLine: true,
-    singleQuote: true,
-    trailingComma: "none",
-    arrowParens: "avoid",
-    semi: true,
-    printWidth: 90,
-}
