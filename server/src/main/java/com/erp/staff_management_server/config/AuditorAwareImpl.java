f5db51b6a433741e29a9a4b217c200bb7dfd37bd
good4ram@naver.com <good4ram@naver.com>
2025-04-14 12:09:17 +0900

    코드 리팩토링 - 서버 코드 정리

--- server/src/main/java/com/erp/staff_management_server/config/AuditorAwareImpl.java
+++ server/src/main/java/com/erp/staff_management_server/config/AuditorAwareImpl.java
@@ -1,4 +1,4 @@
-package com.erp.staff_management_server.util;
+package com.erp.staff_management_server.config;
 import com.erp.staff_management_server.dto.jwt.JwtClaimsDTO;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
@@ -14,10 +14,11 @@ public class AuditorAwareImpl implements AuditorAware<Integer> {
     public Optional<Integer> getCurrentAuditor() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || !authentication.isAuthenticated()) {
-            return Optional.empty();
+            throw new IllegalStateException("인증되지 않은 사용자입니다.");
         }
 
         Object principal = authentication.getPrincipal();
+
         if (principal instanceof JwtClaimsDTO jwtClaimsDTO) {
             return Optional.of(jwtClaimsDTO.getStaffId());
         }
--- server/src/main/java/com/erp/staff_management_server/config/JpaAuditConfig.java
+++ server/src/main/java/com/erp/staff_management_server/config/JpaAuditConfig.java
@@ -1,6 +1,5 @@
 package com.erp.staff_management_server.config;
 
-import com.erp.staff_management_server.util.AuditorAwareImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
