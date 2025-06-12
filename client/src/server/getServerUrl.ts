f1c74b86d7e3c7164063768abd920bd7400366ef
good4ram@naver.com <good4ram@naver.com>
2025-06-12 11:11:02 +0900

    요청 경로에 따른 서버 도메인 변경

--- client/src/server/getServerUrl.ts
+++ client/src/server/getServerUrl.ts
@@ -1,4 +1,12 @@
 export const getServerUrl = (path: string): string => {
-  const host = 'http://localhost:8081';
+  const prefix = path.split('/')[1];
+  let host = '';
+  switch (prefix) {
+    case 'staff' || 'staffs':
+      host = 'http://localhost:8081';
+      break;
+    case 'beneficiary' || 'beneficiaries':
+      host = 'http://localhost:8082';
+  }
   return [host, path].join('');
 };
